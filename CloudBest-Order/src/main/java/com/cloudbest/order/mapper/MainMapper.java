215f7e69bc69fad156adadfd2f8fde9423bd3354
hdq <h_arry@163.com>
2020-07-18 20:00:02 +0800

    限购逻辑

--- CloudBest-Order/src/main/java/com/cloudbest/order/mapper/MainMapper.java
+++ CloudBest-Order/src/main/java/com/cloudbest/order/mapper/MainMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -21,12 +22,18 @@ import java.util.List;
 public interface MainMapper extends BaseMapper<MainEntity> {
 
     //根据主订单号修改订单状态
-    //public int unlockStore(@Param("skuId") Integer skuId, @Param("num") Integer num);
+    public int unlockStore(@Param("skuId") Integer skuId, @Param("num") Integer num);
     @Update("update order_main set pay_status = #{status} where main_order_id =#{id}")
     public int upd(@Param("id") String id, @Param("status") Integer status);
 
     @Select("select * from order_main where user_id = #{id} and pay_status = #{status}")
     public List<MainEntity> selectAllStatus(@Param("id") Long id, @Param("status") Integer status);
+
+    /**
+     * Desc: 根据spuid,skuid,userid  where time , 查询限购频率内的购买数量
+     * @date : 2020/7/18 16:39
+     */
+    Integer selectSumProductQuantity(@Param("param") Map<String,Object> map);
 
 }
 
